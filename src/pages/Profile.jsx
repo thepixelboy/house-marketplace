48df80ec8d34eccccd9608d6c52aabb3c344c97a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-02 11:49:47 +0100

    feat: fetch listings from firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { getAuth, updateProfile } from "firebase/auth";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { db } from "../firebase.config";
 import { updateDoc, doc } from "firebase/firestore";
 import { toast } from "react-toastify";
