4ce1398b5f6cc035c06eb7eeae366f547e76690a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-02 16:48:45 +0100

    feat: listing item component

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,6 +12,7 @@ import {
 import { db } from "../firebase.config";
 import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
+import ListingItem from "../components/ListingItem";
 
 function Category() {
   const [listings, setListings] = useState(null);
@@ -68,7 +69,11 @@ function Category() {
           <main>
             <ul className="categoryListings">
               {listings.map((listing) => (
-                <h3 key={listing.id}>{listing.data.name}</h3>
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                />
               ))}
             </ul>
           </main>
