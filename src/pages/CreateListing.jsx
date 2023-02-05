749e54e346ef2164c6dffccafa2a22ec4feaca8e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-05 11:06:37 +0100

    feat: start create listing page

---
+++ src/pages/CreateListing.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
+
+function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    title: "rent",
+    name: "",
+    bedrooms: 1,
+    bathrooms: 1,
+    parking: false,
+    furnished: false,
+    address: "",
+    offer: false,
+    regularPrice: 0,
+    discountedPrice: 0,
+    images: {},
+    latitude: 0,
+    longitude: 0,
+  });
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({ ...formData, userRef: user.uid });
+        }
+      });
+    } else {
+      navigate("/sign-in");
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return <div>CreateListing</div>;
+}
+export default CreateListing;
