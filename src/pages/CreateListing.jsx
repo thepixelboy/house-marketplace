1c6bf8149acc45be569b033a07b34cae4d1d4467
Ruben <34570952+thepixelboy@users.noreply.github.com>
2023-02-06 17:40:00 +0100

    fix: quick note and change

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -110,7 +110,6 @@ function CreateListing() {
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
-      location = address;
     }
 
     // Store images in Firebase Storage
@@ -169,9 +168,9 @@ function CreateListing() {
       timestamp: serverTimestamp(),
     };
 
+    formDataCopy.location = address;
     delete formDataCopy.images;
     delete formDataCopy.address;
-    location && (formDataCopy.location = location);
     !formDataCopy.offer && delete formDataCopy.discountedPrice;
 
     const docRef = await addDoc(collection(db, "listings"), formDataCopy);
